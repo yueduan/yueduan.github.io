6925bbbf819917025cf69e9503ab958e6ffe5f38
yueduan <bestdyxjtu@gmail.com>
2023-05-27 23:50:41 -0500

    put course files into each dir

---
+++ course_materials/Software Security/lab2/orig.c
@@ -0,0 +1,10 @@
+int main(int argc, char *argv[])
+{
+  int passwd;
+  scanf("%d", &passwd);
+  if (passwd == 3214)
+    printf("Password OK :)\n");
+  else
+    printf("Invalid Password!\n");
+  return 0;
+}
